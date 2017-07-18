7cd31bcc32e6909809c0a6cf477d8c486c7f4759
Rashad Abdul-Salaam <rashad@getskinclick.com>
2017-07-18 00:10:57 -0400

    First Ionic Challenge commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UsersPage } from "../users/users";
+import { ShopPage } from "../shop/shop";
 
 @Component({
   selector: 'page-home',
@@ -11,4 +13,11 @@ export class HomePage {
 
   }
 
+  onGoToUsers() {
+    this.navCtrl.push(UsersPage);
+  }
+
+  goToShopPage() {
+    this.navCtrl.push(ShopPage);
+  }
 }
